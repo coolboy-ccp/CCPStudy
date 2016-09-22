34478b0e1bd6fe89b820a2fdcdd223f45d737b80
brian-chu <chuchengpeng@ccp.local>
2016-09-22 09:36:36 +0800

    Initial Commit

---
+++ CCPStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCPStudy
+//
+//  Created by liqunfei on 16/9/22.
+//  Copyright © 2016年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
